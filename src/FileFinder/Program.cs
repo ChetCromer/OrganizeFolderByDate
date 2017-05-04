462723149f7a222b3378060634b4307b81e7aadd
LAPTOP-UQGR06BE\Vic Finney <vicfinney@c2itconsulting.net>
2017-05-04 16:03:02 -0400

    Got started on writing code.

--- src/FileFinder/Program.cs
+++ src/FileFinder/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace FileFinder
 {
@@ -9,6 +10,18 @@ namespace FileFinder
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Please enter a file name in this format: \"filename\"");
+            string userInput = Console.ReadLine();
+            DriveInfo di = new DriveInfo(@"C:\");
+            DirectoryInfo dirInfo = di.RootDirectory;
+            FileInfo file = dirInfo.EnumerateFiles().SingleOrDefault(x => x.Name == userInput);
+            if (file == null)
+            {
+                Console.WriteLine("Could not find a file with that specific filename.");
+            }
+            else {
+                file.Open(FileMode.Open);
+            }
         }
     }
 }
